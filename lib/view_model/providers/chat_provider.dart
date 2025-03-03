ad7bb832d9e83661e91d95d62e3eb100e9c26e23
0lleZhuk <schov43@ya.ru>
2025-03-03 19:58:52 +0300

    + Провайдер Riverpod для работы с Hive

---
+++ lib/view_model/providers/chat_provider.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive/hive.dart';
+
+import '../../model/message.dart';
+import '../../model/user.dart';
+
+// Провайдер для работы с Hive
+final hiveProvider = Provider((ref) {
+  return {
+    'users': Hive.box<User>('users'),
+    'messages': Hive.box<Message>('messages'),
+  };
+});
+
+// Провайдер для управления состоянием чатов
+final chatProvider = StateNotifierProvider<ChatNotifier, List<User>>((ref) {
+  final hiveBoxes = ref.watch(hiveProvider);
+  return ChatNotifier(hiveBoxes);
+});
+
+class ChatNotifier extends StateNotifier<List<User>> {
+  final Map<String, Box> hiveBoxes;
+
+  ChatNotifier(this.hiveBoxes) : super([]) {
+    loadChats();
+  }
+
+  void loadChats() {
+    final usersBox = hiveBoxes['users'] as Box<User>;
+    state = usersBox.values.toList();
+  }
+
+  Future<void> deleteChat(key) async {
+    final usersBox = hiveBoxes['users'] as Box<User>;
+    await usersBox.delete(key);
+    // print('==> $key');
+    // loadChats(); // Обновляем состояние
+  }
+
+  void clearChat() {
+    final usersBox = hiveBoxes['users'] as Box<User>;
+    usersBox.clear();
+    loadChats(); // Обновляем состояние
+  }
+
+  void sendMessage(User user, String text) {
+    final messagesBox = hiveBoxes['messages'] as Box<Message>;
+    final id = UniqueKey();
+    final message = Message(
+      id,
+      user.lastName,
+      text,
+      '',
+      '',
+      DateTime.now(),
+      true,
+    ); // Исходящее сообщение
+    messagesBox.add(message);
+    // Можно добавить логику для обновления UI
+  }
+}
