82b1e1041be9627669e87dd5d10b2d03e1fe64b7
0lleZhuk <schov43@ya.ru>
2025-03-06 19:47:19 +0300

    Доработка вывода сообщений

--- lib/view/screens/messages_screen.dart
+++ lib/view/screens/messages_screen.dart
@@ -10,23 +10,102 @@ import '../../view_model/providers/chat_provider.dart';
 import '../../view_model/widgets/left_chat_bubble.dart';
 import '../../view_model/widgets/right_chat_bubble.dart';
 
-class MessagesScreen extends ConsumerStatefulWidget {
+class MessagesScreen extends StatelessWidget {
   const MessagesScreen({super.key, required this.user});
 
   final User user;
 
   @override
-  MessagesScreenState createState() => MessagesScreenState();
+  Widget build(BuildContext context) {
+    final firstName = user.firstName;
+    final lastName = user.lastName;
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 80,
+        title: Row(
+          children: [
+            CircleAvatar(
+              minRadius: 24,
+              backgroundColor: user.color,
+              child: Text('${firstName[0]}${lastName[0]}'),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('$firstName $lastName'),
+                const Text('Не в сети', style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          _OutputTextMessages(user: user),
+          _InputTextMessage(user: user),
+        ],
+      ),
+    );
+  }
+
+  /// Метод для определения форм чат-баблов
+  BorderRadius _getBorderRadius(
+    bool isFirstInGroup,
+    bool isLastInGroup,
+    // bool isSingleInGroup,
+  ) {
+    const radius = Radius.circular(26);
+
+    if (isLastInGroup) {
+      // Последнее сообщение в группе
+      return BorderRadius.only(
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: isFirstInGroup ? radius : Radius.zero,
+        bottomRight: radius,
+      );
+    } else if (isFirstInGroup) {
+      // Первое сообщение в группе
+      return const BorderRadius.only(
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: radius,
+      );
+    } else {
+      // Сообщение в середине группы
+      return const BorderRadius.only(
+        topLeft: radius,
+        topRight: radius,
+        bottomLeft: radius,
+        bottomRight: Radius.zero,
+      );
+    }
+  }
 }
 
-class MessagesScreenState extends ConsumerState<MessagesScreen> {
+/// ВЫВОД СООБЩЕНИЙ ===>
+class _OutputTextMessages extends StatefulWidget {
+  const _OutputTextMessages({required this.user});
+
+  final User user;
+
+  @override
+  State<_OutputTextMessages> createState() => _OutputTextMessagesState();
+}
+
+class _OutputTextMessagesState extends State<_OutputTextMessages> {
   final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+      // с учетом реверса - minScrollExtent
+      _scrollController.jumpTo(_scrollController.position.minScrollExtent);
     });
   }
 
@@ -43,133 +122,202 @@ class MessagesScreenState extends ConsumerState<MessagesScreen> {
     // Сортировка сообщений по времени (новые снизу)
     userMessages.sort((a, b) => b.timestamp.compareTo(a.timestamp));
 
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 80,
-        title: Row(
-          children: [
-            CircleAvatar(
-              minRadius: 24,
-              backgroundColor: widget.user.color,
-              child:
-                  Text('${widget.user.firstName[0]}${widget.user.lastName[0]}'),
-            ),
-            const SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('${widget.user.firstName} ${widget.user.lastName}'),
-                const Text(
-                  'Не в сети',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              controller: _scrollController,
-              reverse: true,
-              itemCount: userMessages.length,
-              itemBuilder: (context, index) {
-                final message = userMessages[index];
-                final isMe = message.isOutgoing;
-                final now = message.timestamp;
-                String formattedTime = DateFormat.Hm().format(now);
+    // Группировка сообщений по пользователям, чтобы выявлять цепочки
+    // сообщений, в которых будут отличаться формы чат-баблов
+    final Map<String, List<Message>> groupedMessages = {};
+    for (var message in userMessages) {
+      final userId = message.userId;
+      if (!groupedMessages.containsKey(userId)) {
+        groupedMessages[userId] = [];
+      }
+      groupedMessages[userId]!.add(message);
+    }
 
-                return Column(
-                  crossAxisAlignment:
-                      isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
-                  children: [
-                    ClipPath(
-                      clipper: isMe
-                          ? LeftChatBubble(
-                              largeRadius: largeRadius,
-                              smallRadius: smallRadius,
-                            )
-                          : RightChatBubble(
-                              largeRadius: largeRadius,
-                              smallRadius: smallRadius,
-                            ),
-                      child: Container(
-                        constraints: const BoxConstraints(maxWidth: 300),
-                        // width: 300,
-                        height: 52,
-                        color: isMe
-                            ? theme.colorScheme.secondaryContainer
-                            : widget.user.color.withOpacity(.5),
-                        padding: const EdgeInsets.symmetric(horizontal: 14),
-                        child: isMe
-                            ? Row(
-                                children: [
-                                  const SizedBox(width: 24),
-                                  Text(
-                                    message.text,
-                                    style: const TextStyle(height: 1.3),
-                                    softWrap: true,
-                                  ),
-                                  const Spacer(),
-                                  Text(formattedTime),
-                                  const SizedBox(width: 6),
-                                  if (isMe)
-                                    Image.asset('assets/icons/Read.png'),
-                                ],
-                              )
-                            : Row(
-                                children: [
-                                  Text(
-                                    message.text,
-                                    style: const TextStyle(height: 1.3),
-                                    softWrap: true,
-                                  ),
-                                  const Spacer(),
-                                  Text(formattedTime),
-                                  const SizedBox(width: 6),
-                                  if (isMe)
-                                    Image.asset('assets/icons/Read.png'),
-                                  const SizedBox(width: 20),
-                                ],
-                              ),
+    return Expanded(
+      child: ListView.builder(
+        controller: _scrollController,
+        reverse: true,
+        itemCount: userMessages.length,
+        itemBuilder: (context, index) {
+          final message = userMessages[index];
+          final isMe = message.isOutgoing;
+          final userId = message.userId;
+
+          final messageGroup = groupedMessages[userId]!;
+          final isFirstInGroup = message == messageGroup.first;
+          final isLastInGroup = message == messageGroup.last;
+          // final isSingleInGroup = message == messageGroup.single;
+
+          final now = message.timestamp;
+          String formattedTime = DateFormat.Hm().format(now);
+
+          return Column(
+            // mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment:
+                !isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+            children: [
+              ClipPath(
+                clipper: !isMe
+                    ? LeftChatBubble(
+                        largeRadius: largeRadius,
+                        smallRadius: smallRadius,
+                      )
+                    : RightChatBubble(
+                        largeRadius: largeRadius,
+                        smallRadius: smallRadius,
                       ),
+                child: Container(
+                  constraints: const BoxConstraints(
+                    maxWidth: 300,
+                    minHeight: 52,
+                    maxHeight: double.infinity,
+                  ),
+                  color: isMe
+                      ? theme.colorScheme.secondaryContainer
+                      : widget.user.color.withOpacity(.5),
+                  child: Padding(
+                    padding: !isMe
+                        ? const EdgeInsets.fromLTRB(40, 13, 13, 4)
+                        : const EdgeInsets.fromLTRB(13, 13, 40, 4),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(message.text),
+                        Padding(
+                          padding: isMe
+                              ? const EdgeInsets.only(left: 190)
+                              : const EdgeInsets.only(left: 190),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(formattedTime),
+                              if (isMe)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(bottom: 4, left: 4),
+                                  child: Image.asset('assets/icons/Read.png'),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(height: 4),
-                  ],
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Image.asset('assets/icons/Attach.png'),
-                  onPressed: () {},
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      hintText: 'Сообщение...',
-                    ),
-                    onSubmitted: (text) {
-                      ref
-                          .read(chatProvider.notifier)
-                          .sendMessage(widget.user, text);
-                      Focus.of(context).unfocus();
-                    },
                   ),
                 ),
-                IconButton(
-                  icon: Image.asset('assets/icons/Audio.png'),
-                  onPressed: () {},
+              ),
+              const SizedBox(height: 6)
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+/// ВВОД СООБЩЕНИЙ <===
+/// (нижний ряд)
+
+class _InputTextMessage extends ConsumerStatefulWidget {
+  const _InputTextMessage({required this.user});
+
+  final User user;
+
+  @override
+  InputTextMessageState createState() => InputTextMessageState();
+}
+
+class InputTextMessageState extends ConsumerState<_InputTextMessage> {
+  final _textController = TextEditingController();
+  bool _isButtonVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController.addListener(_updateButtonVisibility);
+  }
+
+  @override
+  void dispose() {
+    _textController.removeListener(_updateButtonVisibility);
+    _textController.dispose();
+    super.dispose();
+  }
+
+  /// Кнопка отправки появляется...
+  void _updateButtonVisibility() {
+    setState(
+      () => _isButtonVisible = _textController.text.isNotEmpty,
+    );
+  }
+
+  /// Метод отправки сообщения:
+  void submitMessage() async {
+    final enteredTextMessage = _textController.text;
+
+    if (enteredTextMessage.trim().isEmpty) return;
+
+    FocusScope.of(context).unfocus();
+    _textController.clear();
+
+    ref
+        .read(chatProvider.notifier)
+        .sendMessage(widget.user, enteredTextMessage);
+  }
+
+  /// Метод отправки сообщения с клавиатуры:
+  // void _onSubmitted(String value) {
+  //   if (value.isNotEmpty) submitMessage();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Image.asset('assets/icons/Attach.png'),
+            onPressed: () {},
+          ),
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              keyboardType: TextInputType.multiline,
+              maxLines: null, // Многострочный режим
+              textCapitalization: TextCapitalization.sentences,
+              decoration: const InputDecoration(
+                // constraints: BoxConstraints(maxWidth: 280),
+                hintText: 'Сообщение...',
+                filled: false,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(26)),
                 ),
-              ],
+              ),
+
+              /// Как возможный вариант - поменять Enter на Done
+              /// для отправки сообщения с клавиатуры
+              // textInputAction: TextInputAction.done,
+              // onSubmitted: _onSubmitted, // Обработчик нажатия
+              /// ------------------------------------
             ),
           ),
+
+          // При наборе текста вместо кн. микрофона появляется кн. Отправить
+          _isButtonVisible
+              ? IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: submitMessage,
+                )
+              : IconButton(
+                  icon: Image.asset(
+                    'assets/icons/Audio.png',
+                    scale: .8,
+                  ),
+                  onPressed: () {},
+                ),
         ],
       ),
     );
