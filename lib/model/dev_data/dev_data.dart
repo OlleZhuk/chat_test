4cf8109db67b6e30f3180a7ee1eda53084c20082
0lleZhuk <schov43@ya.ru>
2025-03-13 15:25:38 +0300

    + Кнопки разработчика на экран чатов

--- lib/model/dev_data/dev_data.dart
+++ lib/model/dev_data/dev_data.dart
@@ -1,9 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../view_model/providers/chat_provider.dart';
 import '../message.dart';
 import '../user.dart';
 
